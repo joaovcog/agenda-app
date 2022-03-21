5cc1f84511bfa8efbcd2f39559255f4ff8e18404
joaovcog <joaovictor.cogomes@gmail.com>
2022-03-21 15:22:25 -0300

    Criacao de formulario reativo para contato

--- src/app/contatos/contatos.component.ts
+++ src/app/contatos/contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContatosService } from '../contatos.service';
 import { Contato } from './contato.model';
 
@@ -8,20 +9,25 @@ import { Contato } from './contato.model';
   styleUrls: ['./contatos.component.css']
 })
 export class ContatosComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor(private contatoService: ContatosService) { }
+  constructor(private contatoService: ContatosService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    const c = new Contato();
-    c.nome = 'José';
-    c.email = 'jose@email.com';
-    c.favorito = false;
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
 
-    this.contatoService.salvar(c).subscribe({
+  submit() {
+    console.log(this.form?.value);
+
+    /*this.contatoService.salvar(c).subscribe({
       next: (response) => {
         console.log(response);
       }
-    });
+    });*/
   }
 
 }
